e876dd15bfd3dfeb7b0332c6b544e52200371e15
Marcos Gómez <margobra8@gmail.com>
2023-03-04 10:26:03 +0100

    initial commit

---
+++ functions/sendgrid.js
@@ -0,0 +1,47 @@
+const client = require("@sendgrid/mail")
+
+function sendEmail(client, message, senderEmail, senderName, toEmail) {
+    return new Promise((fulfill, reject) => {
+        const data = {
+            from: {
+                email: senderEmail,
+                name: senderName
+            },
+            subject: 'Gathered Data: APorTodas',
+            to: toEmail,
+            html: `New form submission<br/> ${message}`
+
+        }
+
+        client
+            .send(data)
+            .then(([response, body]) => {
+                fulfill(response)
+            })
+            .catch(error => reject(error))
+    })
+}
+
+exports.handler = function(event, context, callback) {
+    const {
+        SENDGRID_API_KEY,
+        SENDGRID_SENDER_EMAIL,
+        SENDGRID_SENDER_NAME
+    } = process.env
+
+    const body = JSON.parse(event.body)
+    const message = body.message
+    const destEmail = body.toEmail
+
+    client.setApiKey(SENDGRID_API_KEY)
+
+    sendEmail(
+        client,
+        message,
+        SENDGRID_SENDER_EMAIL,
+        SENDGRID_SENDER_NAME,
+        destEmail
+    )
+    .then(response => callback(null, { statusCode: response.statusCode }))
+    .catch(err => callback(err, null))
+}
